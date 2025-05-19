c262d6388ccc5a5ee93c5517cf8fca1225946c78
Damián Parrino <bucanero@users.noreply.github.com>
2025-05-19 09:39:52 -0300

    Update src/theme/Root.js

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -7,7 +7,7 @@ import Gleap from "gleap"; // See https://gleap.io/docs/javascript/ and https://
 
 function initializeGleap() {
     if (typeof window !== "undefined") {
-        const gleapSdkToken = "K2v3kvAJ5XtPzNYSgk4Ulpe5ptgBkIMv";
+        const gleapSdkToken = "a8VC1kgMViouW0HhfK6aWTrjYEQaXqra";
         // do not check newTab here. Submit code prior to calling this to determine if this is a new tab in the session.
         Gleap.initialize(gleapSdkToken);
         // NEAR-247: Sanitize open-url messages from Gleap
